9bdb5609a7f2dc85f6f9c3e59a7a1c3b788807ee
worlaff <worlaff@gmail.com>
2018-07-24 20:30:58 +0700

    Исправлено преобразование Фурье для N != 2^l. Добавлена возможность произвольного редактирования выделения.

---
+++ OpenCLImageProcessing/Processing/Vector2Extensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+
+namespace Processing
+{
+    public static class Vector2Extensions
+    {
+        public static bool InRadius(this Vector2 self, Vector2 other, float radius)
+        {
+            return (self - other).Length < radius;
+        }
+
+        /// <summary>
+        /// Скалярное произведение
+        /// </summary>
+        /// <param name="self"></param>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public static float DotProduct(this Vector2 self, Vector2 other)
+        {
+            return self.X * other.X + self.Y * other.Y;
+        }
+
+        public static float DistanceTo(this Vector2 self, Vector2 other)
+        {
+            return (self - other).Length;
+        }
+
+        public static float DistanceToSegment(this Vector2 point, Vector2 p0, Vector2 p1)
+        {
+            var v = p1 - p0;
+            var w = point - p0;
+            var c1 = w.DotProduct(v);
+            var c2 = v.DotProduct(v);
+
+            if (c1 < 0)
+                return point.DistanceTo(p0);
+            if (c2 <= c1)
+                return point.DistanceTo(p1);
+
+            var b = c1 / c2;
+            var pb = p0 + v * b;
+
+            return point.DistanceTo(pb);
+        }
+    }
+}
--- OpenCLImageProcessing/UserInterface/DataEditors/Tools/SelectionTool.cs
+++ OpenCLImageProcessing/UserInterface/DataEditors/Tools/SelectionTool.cs
@@ -7,6 +7,7 @@ using Common;
 using Infrastructure;
 using OpenTK;
 using Processing;
+using Processing.Computing;
 using UserInterface.DataEditors.Renderers.Graphics.Selection;
 using UserInterface.DataEditors.Renderers.ImageRenderer;
 using UserInterface.Properties;
@@ -15,29 +16,51 @@ namespace UserInterface.DataEditors.Tools
 {
     public class SelectionTool : ToolBase
     {
+        public enum SelectionEditAction
+        {
+            None,
+            Move,
+            ResizeNW,
+            ResizeNE,
+            ResizeSE,
+            ResizeSW,
+            ResizeN,
+            ResizeE,
+            ResizeS,
+            ResizeW
+        }
+
         public enum SelectionMode
         {
             Square512X512,
             Square1024X1024,
-            Square2048X2048
+            Square2048X2048,
+            Arbitrary2Nx2M,
         }
 
         private const string NewSelectionName = "<Новая область>";
 
         private ToolboxButtonInfo _buttonInfo;
 
-        private ImageSelection _newSelection;   // TODO: можно избавиться от этого
+        private SelectionEditAction _currentSelectionEditAction = SelectionEditAction.None;
+        private ImageSelection _newSelection; // TODO: можно избавиться от этого
         private ImageSelection _currentSelection;
         private SelectionMode _currentSelectionMode = SelectionMode.Square2048X2048;
 
         private ToolStripComboBox _selectionDropdown;
         private ToolStripComboBox _selectionModeDropdown;
         private ToolStripButton _saveSelectionButton;
+        private ToolStripLabel _selectionInfoLabel;
+        private ToolStripTextBox _xTextBox;
+        private ToolStripTextBox _yTextBox;
+        private ToolStripTextBox _widthTextBox;
+        private ToolStripTextBox _heightTextBox;
+        private Vector2 _prevCoord;
 
         private SelectionDrawable _drawable;
         private IReadOnlyCollection<ImageSelection> AvailableSelections => Singleton.Get<ImageSelectionManager>().GetAllSelections();
         public override ToolboxButtonInfo ButtonInfo => _buttonInfo;
-        
+
         private ImageRenderer _renderer;
 
         public SelectionTool(ImageRenderer renderer)
@@ -67,28 +90,132 @@ namespace UserInterface.DataEditors.Tools
 
         public void Save()
         {
-            
         }
 
         public override void MouseEvent(MouseEventData eventData)
         {
-            if ((eventData.Event == MouseEventData.EventType.Down ||
-                 eventData.Event == MouseEventData.EventType.Move)
-                && eventData.Args.Button == MouseButtons.Left)
+            if (_currentSelectionMode != SelectionMode.Arbitrary2Nx2M)
             {
-                if (_currentSelection == _newSelection)
+                if ((eventData.Event == MouseEventData.EventType.Down ||
+                     eventData.Event == MouseEventData.EventType.Move)
+                    && eventData.Args.Button == MouseButtons.Left)
                 {
-                    var imagePos = _renderer.GetImageCoordinate(new Vector2(eventData.Args.X, eventData.Args.Y));
-                    _currentSelection.MoveTo(imagePos);
+                    if (_currentSelection == _newSelection)
+                    {
+                        var imagePos = _renderer.GetImageCoordinate(new Vector2(eventData.Args.X, eventData.Args.Y));
+                        _currentSelection.MoveTo(imagePos);
+                        _drawable.SelectionUpdate();
+                    }
+                }
+            }
+            else
+            {
+                var nw = new Vector2(_currentSelection.X0, _currentSelection.Y0);
+                var sw = new Vector2(_currentSelection.X0, _currentSelection.Y1);
+                var se = new Vector2(_currentSelection.X1, _currentSelection.Y1);
+                var ne = new Vector2(_currentSelection.X1, _currentSelection.Y0);
+
+                var cur = new Vector2(eventData.Args.X, eventData.Args.Y);
+                cur = _renderer.GetImageCoordinate(cur);
+
+                var editRadius = 5f;
+                var action = SelectionEditAction.None;
+
+                if (cur.InRadius(se, editRadius))
+                    action = SelectionEditAction.ResizeSE;
+                else if (cur.InRadius(ne, editRadius))
+                    action = SelectionEditAction.ResizeNE;
+                else if (cur.InRadius(nw, editRadius))
+                    action = SelectionEditAction.ResizeNW;
+                else if (cur.InRadius(sw, editRadius))
+                    action = SelectionEditAction.ResizeSW;
+                else if (cur.DistanceToSegment(nw, ne).Abs() < editRadius)
+                    action = SelectionEditAction.ResizeN;
+                else if (cur.DistanceToSegment(ne, se).Abs() < editRadius)
+                    action = SelectionEditAction.ResizeE;
+                else if (cur.DistanceToSegment(se, sw).Abs() < editRadius)
+                    action = SelectionEditAction.ResizeS;
+                else if (cur.DistanceToSegment(nw, sw).Abs() < editRadius)
+                    action = SelectionEditAction.ResizeW;
+                else if (_currentSelection.GetRectangle().ContainsPoint((int) cur.X, (int) cur.Y))
+                    action = SelectionEditAction.Move;
+
+                if (action == SelectionEditAction.ResizeNW || action == SelectionEditAction.ResizeSE)
+                    Cursor.Current = Cursors.SizeNWSE;
+                else if (action == SelectionEditAction.ResizeNE || action == SelectionEditAction.ResizeSW)
+                    Cursor.Current = Cursors.SizeNESW;
+                else if (action == SelectionEditAction.ResizeN || action == SelectionEditAction.ResizeS)
+                    Cursor.Current = Cursors.SizeNS;
+                else if (action == SelectionEditAction.ResizeW || action == SelectionEditAction.ResizeE)
+                    Cursor.Current = Cursors.SizeWE;
+                else if (action == SelectionEditAction.Move)
+                    Cursor.Current = Cursors.SizeAll;
+                else
+                    Cursor.Current = Cursors.Default;
+
+                if (eventData.Event == MouseEventData.EventType.Down && eventData.Args.Button == MouseButtons.Left)
+                {
+                    _prevCoord = cur;
+                    _currentSelectionEditAction = action;
+                }
+
+                if (eventData.Event == MouseEventData.EventType.Up && eventData.Args.Button == MouseButtons.Left)
+                {
+                    _currentSelectionEditAction = SelectionEditAction.None;
+                }
+
+                if (eventData.Event == MouseEventData.EventType.Move && eventData.Args.Button == MouseButtons.Left)
+                {
+                    switch (_currentSelectionEditAction)
+                    {
+                        case SelectionEditAction.ResizeNW:
+                            _currentSelection.X0 = (int) cur.X;
+                            _currentSelection.Y0 = (int) cur.Y;
+                            break;
+                        case SelectionEditAction.ResizeSW:
+                            _currentSelection.X0 = (int) cur.X;
+                            _currentSelection.Y1 = (int) cur.Y;
+                            break;
+                        case SelectionEditAction.ResizeSE:
+                            _currentSelection.X1 = (int) cur.X;
+                            _currentSelection.Y1 = (int) cur.Y;
+                            break;
+                        case SelectionEditAction.ResizeNE:
+                            _currentSelection.X1 = (int) cur.X;
+                            _currentSelection.Y0 = (int) cur.Y;
+                            break;
+                        case SelectionEditAction.ResizeN:
+                            _currentSelection.Y0 = (int) cur.Y;
+                            break;
+                        case SelectionEditAction.ResizeS:
+                            _currentSelection.Y1 = (int) cur.Y;
+                            break;
+                        case SelectionEditAction.ResizeW:
+                            _currentSelection.X0 = (int) cur.X;
+                            break;
+                        case SelectionEditAction.ResizeE:
+                            _currentSelection.X1 = (int) cur.X;
+                            break;
+                        case SelectionEditAction.Move:
+                            _currentSelection.MoveBy(cur - _prevCoord);
+                            _prevCoord = cur;
+                            break;
+                    }
+
+                    _currentSelection.Width = _currentSelection.Width - _currentSelection.Width % 2;
+                    _currentSelection.Height = _currentSelection.Height - _currentSelection.Height % 2;
                     _drawable.SelectionUpdate();
                 }
             }
+
+            UpdateInfoLabel();
         }
 
         public override void Activate()
         {
             Cursor.Current = Cursors.Cross;
             _renderer.AddDrawable(_drawable);
+            _drawable.SelectionUpdate();
         }
 
         public override void Deactivate()
@@ -111,8 +238,8 @@ namespace UserInterface.DataEditors.Tools
                 _drawable.SelectionUpdate();
             };
 
-            _selectionModeDropdown =new ToolStripComboBox("Selection Mode");
-            _selectionModeDropdown.Items.AddRange(EnumExtensions.GetValues<SelectionMode>().Select(v => (object)v).ToArray());
+            _selectionModeDropdown = new ToolStripComboBox("Selection Mode");
+            _selectionModeDropdown.Items.AddRange(EnumExtensions.GetValues<SelectionMode>().Select(v => (object) v).ToArray());
             _selectionModeDropdown.DropDownStyle = ComboBoxStyle.DropDownList;
             _selectionModeDropdown.SelectedItem = _currentSelectionMode;
             _selectionModeDropdown.SelectedIndexChanged += (sender, args) =>
@@ -143,9 +270,22 @@ namespace UserInterface.DataEditors.Tools
                 _selectionDropdown.SelectedItem = _currentSelection;
             };
 
+            _selectionInfoLabel = new ToolStripLabel();
+
             toolStrip.Items.Add(_selectionDropdown);
             toolStrip.Items.Add(_selectionModeDropdown);
             toolStrip.Items.Add(_saveSelectionButton);
+            toolStrip.Items.Add(_selectionInfoLabel);
+        }
+
+        private void UpdateInfoLabel()
+        {
+            var m1 = Fourier.OddDenominator(_currentSelection.Width);
+            var m2 = Fourier.OddDenominator(_currentSelection.Height);
+            var l1 = _currentSelection.Width / m1;
+            var l2 = _currentSelection.Height / m2;
+
+            _selectionInfoLabel.Text = $"{_currentSelection.CoordsToString()} [M {m1},L {l1}] x [M {m2}, L{l2}]";
         }
     }
 }
