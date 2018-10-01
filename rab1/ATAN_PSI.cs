d061e7533754775cc1540ed92888b715fa82e484
Guzhov Vladimir <vigguzhov@gmail.com>
2018-10-01 16:43:56 +0700

    мОДЕЛИРОВАНИЕ СЛУЧАЙНОГО СДВИГА ФАЗ В ГИ (Гужов)

--- rab1/ATAN_PSI.cs
+++ rab1/ATAN_PSI.cs
@@ -46,7 +46,8 @@ namespace rab1.Forms
             // MessageBox.Show("width= " + w1 + "height= " + h1);
             MessageBox.Show("fzz[i]= " + fzz[0] + "  " + fzz[1] + "  " + fzz[2] + "  " + fzz[3]);
 
-            double Ar = SumClass.getAverage(zArrayPicture[8]);
+            //double Ar = SumClass.getAverage(zArrayPicture[8]);
+            double Ar = amplit;
 
             ZComplexDescriptor faza = new ZComplexDescriptor(w1, h1);
 
@@ -94,18 +95,16 @@ namespace rab1.Forms
                     //double fz1 = Vector_Mul(i_sdv, sin_orto);
                     //double fz2 = Vector_Mul(i_sdv, cos_orto);]
 
-                    double a = Math.Atan2(fz2, fz1);
+                    double f = Math.Atan2(fz2, fz1);
 
                     //double[] cos_orto = Vector_orto(k_cos);              // ------  Формула расшифровки амплитуды                   
                     //double znmt = Vector_Mul(sin_orto, k_cos);
                     double am = Math.Sqrt(fz1 * fz1 + fz2 * fz2) / znmt;
                     //am = am / (2 * amplit);
                     am = am / (2 * Ar);
-                    //a= ampf.array[i, j];
-                    // am = amp.array[i, j];
-                    //double am = 255;
+                   
 
-                    faza.array[i, j] = Complex.FromPolarCoordinates(am, a);
+                    faza.array[i, j] = Complex.FromPolarCoordinates(am, f);
                     
                 }
                 progressBar1.PerformStep();
