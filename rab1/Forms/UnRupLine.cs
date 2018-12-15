1afd8796bf84aee7f341e5b2c792097518a14fef
Guzhov Vladimir <vigguzhov@gmail.com>
2018-12-15 11:46:55 +0700

    UnRupLine

---
+++ rab1/Forms/UnRupLine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+public delegate void DelegatUnrupLine(int x0, double gr);
+
+
+namespace rab1.Forms
+{
+    public partial class UnRupLine : Form
+    {
+        private static double gr1 = -2.1;
+        private static double gr2 = -2.1;
+        private static int x0 = 4000;
+
+        public event DelegatUnrupLine OnUnrupLine;
+        public event DelegatUnrupLine OnUnrupLinePlus;
+
+        public UnRupLine()
+        {
+            InitializeComponent();
+            textBox4.Text = Convert.ToString(gr1);
+            textBox1.Text = Convert.ToString(gr2);
+            textBox2.Text = Convert.ToString(x0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)  // Развертка по строкам
+        {
+            x0  = Convert.ToInt32(textBox2.Text);
+            gr1 = Convert.ToDouble(textBox4.Text);
+            OnUnrupLine(x0, gr1);
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            x0 = Convert.ToInt32(textBox2.Text);
+            gr2 = Convert.ToDouble(textBox1.Text);
+            OnUnrupLinePlus(x0, gr2);
+            Close();
+        }
+    }
+}
