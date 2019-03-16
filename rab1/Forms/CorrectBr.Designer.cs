6fe43ae025306f779d41a8d9afad3e853f406db6
Guzhov Vladimir <vigguzhov@gmail.com>
2019-03-16 10:30:48 +0700

    ---

---
+++ rab1/Forms/CorrectBr.Designer.cs
@@ -0,0 +1,443 @@
+﻿namespace rab1.Forms
+{
+    partial class CorrectBr
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(245, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выравнивание по длине массива \r\nот X1 до X2 => 0 до Размер нового массива";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(356, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(37, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(285, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "k (1-12) =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(650, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Размер нового массива =";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(824, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "4096";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(472, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "=>";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(508, 33);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(37, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "2";
+            // 
+            // button2
+            // 
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button2.Location = new System.Drawing.Point(13, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(244, 53);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "В к1 Профиль клина с гамма-искажениями\r\nв к2 Профиль клина\r\nв к3 Профиль с обратн" +
+    "ой кривизной";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(418, 95);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(37, 20);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.Text = "2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(285, 98);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "k (1-12) =";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(356, 95);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(37, 20);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(472, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "=>";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(508, 95);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(37, 20);
+            this.textBox6.TabIndex = 13;
+            this.textBox6.Text = "5";
+            // 
+            // button3
+            // 
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button3.Location = new System.Drawing.Point(12, 152);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(244, 25);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Клин от I0 до 255 Ширина Размер+200";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(285, 156);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "I0 =";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(356, 156);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(99, 20);
+            this.textBox7.TabIndex = 16;
+            this.textBox7.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(472, 159);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(19, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "=>";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(508, 156);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(37, 20);
+            this.textBox8.TabIndex = 18;
+            this.textBox8.Text = "1";
+            // 
+            // button4
+            // 
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button4.Location = new System.Drawing.Point(13, 194);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(244, 25);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Сложение строк от Y1 до Y2";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(357, 195);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(37, 20);
+            this.textBox11.TabIndex = 25;
+            this.textBox11.Text = "1";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(472, 198);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(19, 13);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "=>";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(285, 198);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(52, 13);
+            this.label11.TabIndex = 26;
+            this.label11.Text = "k (1-12) =";
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(508, 195);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(37, 20);
+            this.textBox12.TabIndex = 31;
+            this.textBox12.Text = "2";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(13, 239);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(245, 41);
+            this.button5.TabIndex = 32;
+            this.button5.Text = "Строка Line из массива k1 растягивается\r\nдобавляется полосами слева и справа";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(285, 255);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 13);
+            this.label8.TabIndex = 34;
+            this.label8.Text = "k (1-12) =";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(357, 252);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(37, 20);
+            this.textBox9.TabIndex = 33;
+            this.textBox9.Text = "1";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(601, 257);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(36, 13);
+            this.label9.TabIndex = 40;
+            this.label9.Text = "Line =";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(508, 254);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(38, 20);
+            this.textBox10.TabIndex = 39;
+            this.textBox10.Text = "2";
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(672, 254);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(86, 20);
+            this.textBox13.TabIndex = 42;
+            this.textBox13.Text = "128";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(472, 257);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(19, 13);
+            this.label12.TabIndex = 41;
+            this.label12.Text = "=>";
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(824, 251);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(100, 20);
+            this.textBox14.TabIndex = 44;
+            this.textBox14.Text = "4096";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(797, 226);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(140, 13);
+            this.label13.TabIndex = 43;
+            this.label13.Text = "Размер нового массива =";
+            // 
+            // textBox15
+            // 
+            this.textBox15.Location = new System.Drawing.Point(824, 286);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.Size = new System.Drawing.Size(100, 20);
+            this.textBox15.TabIndex = 45;
+            this.textBox15.Text = "2160";
+            // 
+            // CorrectBr
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 450);
+            this.Controls.Add(this.textBox15);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "CorrectBr";
+            this.Text = "Коррекция неравномерности освещения";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox15;
+    }
+}
---
+++ rab1/Forms/CorrectBr.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rab1.Forms
+{
+    public delegate void CorrectBr1(int k1, int k2, int n);
+    public delegate void CorrectBr2(int k1, int k2);
+    public delegate void CorrectBr3(int k1, int k2, int N, int nx, int ny);
+    public partial class CorrectBr : Form
+    {
+        public event CorrectBr1 On_CorrectX;
+        public event CorrectBr1 On_CorrectG;
+        public event CorrectBr1 On_CorrectClin;
+        public event CorrectBr2 On_CorrectSumm;
+        public event CorrectBr3 On_CorrectGxy;
+
+        private static int n = 4096;                  // Размер массива
+        private static int k1 = 1;
+        private static int k2 = 2;
+
+        private static int k11 = 1;
+        private static int k21 = 2;
+        private static int k31 = 5;
+
+        private static int k12 = 1;
+        private static int I0 = 0;
+
+        private static int k13 = 1;
+        private static int k23 = 2;
+
+        private static int k14 = 1;
+        private static int k24 = 2;
+        private static int N_Line = 128;
+        private static int nx = 4096;                  // Размер массива
+        private static int ny = 2160;                  // Размер массива
+        public CorrectBr()
+        {
+            InitializeComponent();
+            textBox1.Text = Convert.ToString(k1);     // Номер кадра
+            textBox3.Text = Convert.ToString(k2);     // Номер кадра
+            textBox2.Text = Convert.ToString(n);      // Размер массива
+
+            textBox4.Text = Convert.ToString(k11);
+            textBox5.Text = Convert.ToString(k21);
+            textBox6.Text = Convert.ToString(k31);
+
+            textBox7.Text = Convert.ToString(I0);
+            textBox8.Text = Convert.ToString(k12);
+
+            textBox11.Text = Convert.ToString(k13);    // Сложение строк
+            textBox12.Text = Convert.ToString(k23);
+
+            textBox9.Text = Convert.ToString(k14);    // Сложение строк
+            textBox10.Text = Convert.ToString(k24);
+            textBox13.Text = Convert.ToString(N_Line);
+            textBox14.Text = Convert.ToString(nx);
+            textBox15.Text = Convert.ToString(ny);
+        }
+        /// <summary>
+        /// Массив меняет длину от X1 до X2 -> 0 до 4096      
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)    // Button1 "Выравнивание по длине массива"
+        {
+            k1 = Convert.ToInt32(textBox1.Text);
+            k2 = Convert.ToInt32(textBox3.Text);
+            n = Convert.ToInt32(textBox2.Text);
+
+            On_CorrectX(k1-1, k2-1, n);
+            Close();
+        }
+        /// <summary>
+        /// Определения клина с обратными гамма-искажениями
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            k11 = Convert.ToInt32(textBox4.Text);
+            k21 = Convert.ToInt32(textBox5.Text);
+            k31 = Convert.ToInt32(textBox6.Text);
+            On_CorrectG(k11 - 1, k21 - 1, k31-1);
+            Close();
+        }
+        /// <summary>
+        /// Клин от I0 до 255
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            k12 = Convert.ToInt32(textBox8.Text);
+            I0 = Convert.ToInt32(textBox7.Text);
+            n = Convert.ToInt32(textBox2.Text);
+            On_CorrectClin(I0, n, k12 - 1);
+            Close();
+
+
+        }
+        /// <summary>
+        /// Сложение строк от Y1 до Y2
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button4_Click(object sender, EventArgs e)
+        {
+            k13 = Convert.ToInt32(textBox11.Text);
+            k23 = Convert.ToInt32(textBox12.Text);
+            On_CorrectSumm(k13-1, k23 - 1);
+            Close();
+
+        }
+        /// <summary>
+        ///  Растяжение массива для проекции
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button5_Click(object sender, EventArgs e)
+        {
+           // textBox9.Text = Convert.ToString(k14);    // Сложение строк
+          
+            k14 = Convert.ToInt32(textBox9.Text);
+            k24 = Convert.ToInt32(textBox10.Text);
+            N_Line = Convert.ToInt32(textBox13.Text);
+            nx= Convert.ToInt32(textBox14.Text);
+            ny = Convert.ToInt32(textBox15.Text);
+            On_CorrectGxy(k14 - 1, k24 - 1, N_Line, nx, ny);
+            Close();
+
+        }
+    }
+}
